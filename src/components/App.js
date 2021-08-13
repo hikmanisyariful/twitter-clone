0efe35bceed6d6721ba470116a25239c6fad016d
hikmanisyariful <syarifulfajar017@gmail.com>
2021-08-13 14:31:29 +0700

    handle initial data in App Component using connect react redux

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,18 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { handleInitialData } from "../actions/shared";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Twitter Clone</h1>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Twitter Clone</h1>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default connect()(App);
