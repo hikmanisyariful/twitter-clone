a63133598f4bd22af1fb1d47912043ae17626089
hikmanisyariful <syarifulfajar017@gmail.com>
2021-08-13 15:02:29 +0700

    find TweetIds using mapStateToProps

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
 import { handleInitialData } from "../actions/shared";
+import Dashboard from "./Dashboard";
 
 class App extends Component {
   componentDidMount() {
@@ -9,7 +10,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>Twitter Clone</h1>
+        <Dashboard />
       </div>
     );
   }
